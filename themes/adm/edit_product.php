9b11d08318a88fa7ca6464a89bcce89d5c7b7a93
Maria Eduarda Teixeira Jungbluth <maria.jungbluth@icloud.com>
2024-10-25 08:59:24 -0300

    commit novo

--- themes/adm/edit_product.php
+++ themes/adm/edit_product.php
@@ -10,59 +10,30 @@
     <title>Editar Produtos</title>
     <link rel="stylesheet" href="<?= url("assets/css/adm/edit_product.css"); ?>">
     <script type= "module" src="<?= url("assets/js/adm/table.js"); ?>" async></script>
+    
 </head>
 <body>
 <main>
-    <div class="container">
-        <div class="form-products">
-            <form id="form-edit">
-                <h2>Editar Produto</h2>
-                <input type="text" name="id" placeholder="ID do produto">
-                <input type="text" name="name" placeholder="Alterar nome">
-                <input type="number" name="price" placeholder="Alterar Valor">
-                <input type="number" name="amount" placeholder="Alterar quantidade">
-                <input type="text" name="url_products" placeholder="Alterar URL da Imagem">
-                <input type="text" name="description" placeholder="Alterar Descrição">
-                <input type="text" name="categories_id" placeholder="Alterar Categoria">
-                <input type="submit" value="Editar Produto">
-                
-                <div id="toast-container"></div>
-            </form>
-        </div>
-
-        <div class="form-products">
-    <div class="tabelaprodutos">
-        <h1>TABELA DE PRODUTOS CADASTRADOS:</h1>
-
+<div class="container">
         <div class="container-search">
             <form class="formSearch">
-                <h3>Pesquise o id aqui</h3>
+                <h3>Não encontrou o produto? Pesquise aqui</h3>
                 <input type="number" name="product_id" placeholder="Digite o ID do produto a encontrar">
-                <input type="submit" value="Pesquisar">
+                <input type="submit" value="Encontrar agora!">
             </form>
             <span class="message-product"></span>
         </div>
-
-        <table class="tabela">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Preço</th>
-                    <th>Quantidade</th>
-                    <th>URL produto</th>
-                    <th>Descrição do produto</th>
-                    <th>Categoria do produto</th>
-                </tr>
-            </thead>
-            <tbody id="tabela-corpo"></tbody>
-        </table>
-
-        <div class="toast-container"></div>
-    </div>
-</div>
-
+        <div class="container-edit">
+            <h2>Editar Produtos</h2>
         </div>
+        <div class="container-table">
+            <table>
+                <tbody>
+                    <!-- Conteúdo vindo do Banco -->
+                </tbody>
+            </table>
+        </div>
+        <div class="toast-container"></div>
     </div>
 </main>
 
